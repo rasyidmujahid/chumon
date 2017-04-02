f51bfe94b554b25a48eb68fbb257a48bb3778844
Abdurrasyid Mujahid <rasyidmujahid@gmail.com>
2017-04-02 20:01:59 +0700

    initial commit

---
+++ src/main/java/com/chumon/ChumonApplication.java
@@ -0,0 +1,12 @@
+package com.chumon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChumonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChumonApplication.class, args);
+	}
+}
